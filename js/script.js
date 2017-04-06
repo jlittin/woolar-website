8d7e747327e39a65c2a8d025f29676712eb47fc4
jlittin <josh@huffle.com.au>
2017-04-06 11:59:37 +0800

    Hamburger Menu and Mobile Optimisation
    Imported javascript and updated some of the break points.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  $(".burger-button").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").slideToggle();
+   });
+
+
+  $(".burger-menu").click(function(){
+    $(".burger-button").toggleClass("active");
+    $(".burger-menu").slideToggle();
+  });
+
+})
